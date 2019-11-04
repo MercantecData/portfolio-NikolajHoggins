c3014670f2312ce3cc61f78943aea9b8a033a4ec
Nikolaj Hoggins Jacobsen <nikolajhjacobsen@gmail.com>
2019-11-04 14:08:16 +0100

    hurtigt lille program Afleveret

--- lilletalting/Program.cs
+++ lilletalting/Program.cs
@@ -6,17 +6,46 @@ namespace lilletalting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Her har du hvad opgaven siger (0,1,2,3,4,5,6) og hvad eksemplet siger (1,2,3,4,5,6)
+            var returnArray = returnFullArrayFrom0(6);
+            var returnArray1 = returnFullArrayFrom1(6);
+
+            Console.WriteLine("FROM 0");
+            foreach (var item in returnArray)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.WriteLine("FROM 1");
+            foreach (var item in returnArray1)
+            {
+                Console.WriteLine(item);
+            }
         }
 
-        static int[] returnFullArrray(int number)
+        static int[] returnFullArrayFrom1(int number)
         {
             int[] array = new int[number];
 
             for (int i = 0; i < array.Length; i++)
             {
-
+                array[i] = i + 1;
             }
+
+            return array;
+        }
+
+        static int[] returnFullArrayFrom0(int number)
+        {
+            int[] array = new int[number+1];
+
+            for (int i = 0; i <= array.Length-1; i++)
+            {
+                array[i] = i;
+            }
+
+            return array;
         }
     }
 }
