a46e650eab1bd2f6a68b88566a6a3e169417c28f
Nikolaj Hoggins <mollevip@gmail.com>
2019-11-28 15:34:12 +0100

    Yderligere arbejde på Department sql

--- DatabaseProgrammering/departmentdb/database.sql
+++ DatabaseProgrammering/departmentdb/database.sql
@@ -1,13 +1,17 @@
+/*Set up database, but delete first if exists.*/
 DROP DATABASE IF EXISTS departmentdb;
 CREATE DATABASE departmentdb;
 
 USE departmentdb;
+
+/*Salaries is set up with the salary, and is to be used to define titles salaries*/
 CREATE TABLE salaries(
     sal_id int AUTO_INCREMENT,
     monthly_salary double,
     PRIMARY KEY (sal_id)
 );
 
+/*Titles have a salary in them, a title name and is to be used on an employee.*/
 CREATE TABLE titles(
     title_id int AUTO_INCREMENT,
     sal_id int,
@@ -16,6 +20,8 @@ CREATE TABLE titles(
     FOREIGN KEY (sal_id) 
         REFERENCES salaries(sal_id)
 );
+
+/**/
 CREATE TABLE employee(
     employee_id int AUTO_INCREMENT,
     title_id int,
@@ -51,4 +57,37 @@ CREATE TABLE dep_employee(
     FOREIGN KEY (dep_id) REFERENCES department(dep_id)
 );
 
+/*Creating a few departments*/
+INSERT INTO department(dep_name) VALUES
+('Texas Department'),
+('Minnesota Department'),
+('New York Department');
 
+/**/
+INSERT INTO salaries(monthly_salary) VALUES
+(11300.00),
+(24000.00),
+(38000.00),
+(56000.00),
+(72000.00);
+
+INSERT INTO titles(title_name, sal_id) VALUES
+('Lead Dev', 4),
+('Programmer', 3),
+('Programmer Apprentience', 1),
+('Janitor', 2),
+('Manager', 5),
+('It-supporter', 2);
+
+INSERT INTO employee(title_id, employee_name, employee_age, employee_address, employed_years) VALUES
+((SELECT title_id FROM titles WHERE title_name = 'Lead Dev'), 'Rasmus Kjeldsen', 26, 'Normansvej 1, 8900, Randers', 8),
+((SELECT title_id FROM titles WHERE title_name = 'Programmer Apprentience'), 'Nikolaj Hoggins', 17, 'Hans Tausens Vej 22, 8800, Viborg', 1),
+(5, 'Mike Møller', 29, 'Vejnavn 14, 8900, Randers', 5),
+(4, 'Per Persen', 54, 'Brovej 43, 8900, Randers', 17),
+(2, 'Progra Møhr', 22, 'Kodevej 7, 1113, København', 3);
+
+
+SELECT * FROM department;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+SELECT * FROM employee;
